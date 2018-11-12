f1b9be207e6988dca9cd23ca013a90ad8b2a3ad7
韩雨清 <hanyuqing@kiyoshis-MacBook-puro.local>
2018-11-11 19:50:54 -0500

    add Navigation bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = ( props ) => (
     <>
-        <div>ToolBar, SideDrawer, BackDrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
