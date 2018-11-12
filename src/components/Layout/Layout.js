d7b704a89fb708365e7b78287f38f232a95643ca
yukeehan <yukeehan@gmail.com>
2018-11-12 14:19:53 -0500

    sidedrawer closed achieved

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </>
-)
+class Layout extends Component {
+    state={
+        showSideDrawer: true
+    }
 
-export default layout;
+    SideDrawerClosedHandler = () =>{
+        this.setState({showSideDrawer: false});
+    }
+
+    render() {
+        return(
+            <>
+                <Toolbar />
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.SideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </>
+        )
+    }
+}
+
+export default Layout;
---
+++ src/js/actions/index.js
@@ -0,0 +1,9 @@
+// Action
+import { ADD_ARTICLE } from '../constants/action-types';
+
+export const addArticle = article => (
+    {
+        type:  ADD_ARTICLE, 
+        payload: article
+    }
+);
